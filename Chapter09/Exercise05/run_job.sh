bd8177586640bd45313188bb5b1741f99b7c9efe
Kevin Liao <lwk723@berkeley.edu>
2019-12-30 17:55:19 -0800

    add exercise 5

---
+++ Chapter09/Exercise05/run_job.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# set -e
+
+# set config
+DATE=17.14.11
+SOURCE_FILE=../Data/USvideos.csv.zip
+CAT_FILE=../Data/US_category_id.json
+OUTPUT_FILE=../Data/top_10_trendy_cats.csv
+SRC_DIR=../Exercise03
+
+echo "[[ JOB ]] runs on date $DATE with file located in $SOURCE_FILE and meta data located in $CAT_FILE"
+echo "[[ JOB ]] result data will be persisted in $OUTPUT_FILE"
+
+# run job
+echo "[[ RUNNING JOB ]] step 1: filter source data"
+python $SRC_DIR/filter_data.py --file $SOURCE_FILE --date $DATE &
+
+echo "[[ RUNNING JOB ]] step 1.1: preprcess meta data"
+python $SRC_DIR/preprocess_data.py --file $CAT_FILE &
+
+echo "[[ BLOCKING JOB ]] addtional step: check cached files and trigger next step"
+while [[ ! -f ./tmp/data_cats.csv ]] || [[ ! -f ./tmp/data_vids.csv ]]
+do
+  sleep 1
+done
+
+echo "[[ RUNNING JOB ]] step 2: join data"
+python $SRC_DIR/join_data.py
+
+echo "[[ RUNNING JOB ]] step 3: rank categories"
+python $SRC_DIR/sort_data.py
+
+echo "[[ RUNNING JOB ]] step 4: persist result data"
+python $SRC_DIR/store_data.py --path $OUTPUT_FILE
+
+if [ "$?" = "0" ]; then
+	rm -rf ./tmp/
+	echo "[[ JOB ]] END"
+else
+	rm -rf ./tmp/
+	echo "[[ JOB FAILS!! ]]" 1>&2
+	exit 1
+fi
