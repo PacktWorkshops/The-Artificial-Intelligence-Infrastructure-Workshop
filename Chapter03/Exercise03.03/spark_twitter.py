8dbc747ffde2461737f6a3336c4c09dd5f164e75
Bas Geerdink <bas@gdink.com>
2020-02-12 21:12:48 +0100

    showing all tweets in append mode to console

--- Chapter03/Exercise03.03/spark_twitter.py
+++ Chapter03/Exercise03.03/spark_twitter.py
@@ -8,23 +8,21 @@ spark = SparkSession.builder.appName('Packt').getOrCreate()
 lines = spark.readStream.format('socket').option('host', 'localhost').option('port', 5557).load()
 
 
-schema = StructType([StructField("created_at", TimestampType(), True),
-                    StructField("text", StringType(), True)])
+schema = StructType([StructField('created_at', StringType(), True),
+                    StructField('text', StringType(), True)])
 
-lines2 = lines.select(from_json(lines.value, schema).alias('tweet'))
+lines2 = lines.select(from_json('value', schema).alias('tweet'))
 
 lines3 = lines2.selectExpr('tweet.created_at', 'tweet.text')
 
-lines4 = lines3.withColumn("count", lit(1))
+lines4 = lines3.withColumn('count', lit(1))
 
-windowed = lines4.groupBy(window("created_at", "1 minute", "10 seconds"))
+query = lines4.writeStream.outputMode('append').format('console').start()
 
-counts = windowed.count()
+#windowed = lines4.groupBy(window('created_at', '1 minute', '10 seconds'))
 
-# words = lines.select(explode(split(lines.value, " ")).alias('word'))
+#counts = windowed.count()
 
-# wordCounts = words.groupBy('word').count()
-
-query = counts.writeStream.outputMode('complete').format('console').start()
+#query = counts.writeStream.outputMode('complete').format('console').start()
 
 query.awaitTermination()
