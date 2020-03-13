cbcc5d6b33c5bf402905281d8f850b3c9d15b29a
Bas Geerdink <bas@gdink.com>
2020-03-13 21:31:09 +0100

    added unit test for Exercise 12.01

--- Chapter12/Exercise12.01/production.py
+++ Chapter12/Exercise12.01/production.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
 import pickle
 
 # load the model from pickle file
@@ -6,17 +6,23 @@ file = open('model.pkl', 'rb')  # read bytes
 model = pickle.load(file)
 file.close()
 
+# get predictions from the model
+print(model.predict([[3, 0, 22.0, 1, 0, 7.25]]))  # male
+print(model.predict([[3, 1, 22.0, 1, 0, 7.25]]))  # female
+
 # create an API with Flask
 app = Flask('Titanic')
 
 
+# call this: curl -X GET http://127.0.0.1:5000/foo
 @app.route('/hi', methods=['GET'])
-def hello():
-    return 'Hello world!'
+def bar():
+    result = 'hello!'
+    return result
 
 
-# call this: curl -X POST -H "Content-Type: application/json" \
-# -d '{"Pclass": 3, "Sex": 0, "Age": 72, "SibSb": 2, "Parch": 0, "Fare": 8.35}' http://127.0.0.1:5000/survived
+# call this: curl -X POST -H "Content-Type: application/json" -d \
+# '{"Pclass": 3, "Sex": 0, "Age": 72, "SibSb": 2, "Parch": 0, "Fare": 8.35}' http://127.0.0.1:5000/survived
 @app.route('/survived', methods=['POST'])
 def survived():
     payload = request.get_json()
