fec2f2df387893010f472ea8d0a974e390c4f694
Bas Geerdink <bas@gdink.com>
2020-03-13 21:33:25 +0100

    updated code and unit test for Exercise 12.03

---
+++ Chapter12/Exercise12.03/titanic_survived/src/test/java/packt/StreamingJobTests.java
@@ -0,0 +1,36 @@
+package packt;
+
+import jdk.internal.util.xml.impl.Input;
+import org.jpmml.evaluator.InputField;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StreamingJobTests {
+
+    @Test
+    public void canDeserializePmmlModel() {
+        // arrange
+
+        // act
+
+        // assert
+        System.out.println("OK");
+    }
+
+    @Test
+    public void canGetFieldMap() {
+        // arrange
+        StreamingJob sj = new StreamingJob();
+
+        // act
+
+        // assert
+
+        System.out.println("OK");
+    }
+
+}
