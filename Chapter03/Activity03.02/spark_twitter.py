009d6f92df1e9dd0b783645839897cb1c968116c
ashishjPackt <56333060+ashishjPackt@users.noreply.github.com>
2020-04-02 01:45:13 +0530

    Minor Updates in sequence

--- Chapter03/Activity03.02/spark_twitter.py
+++ Chapter03/Activity03.02/spark_twitter.py
@@ -3,24 +3,26 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, window, to_timestamp, explode, split, col
 from pyspark.sql.types import StructType, StructField, StringType
 
-tweet_datetime_format = 'EEE MMM dd HH:mm:ss ZZZZ yyyy'
+# 3
+spark = SparkSession.builder.appName('Packt').getOrCreate()
 
+# 4
+raw_stream = spark.readStream.format('socket').option('host', 'localhost').option('port', 1234).load()
+
+# 5
+tweet_datetime_format = 'EEE MMM dd HH:mm:ss ZZZZ yyyy'
 schema = StructType([StructField('created_at', StringType(), True),
                      StructField('text', StringType(), True)])
-
-spark = SparkSession.builder.appName('Packt').getOrCreate()
-# 3
-raw_stream = spark.readStream.format('socket').option('host', 'localhost').option('port', 1234).load()
-# 4
+# 6
 tweet_stream = raw_stream.select(from_json('value', schema).alias('tweet'))
-# 5
+
+# 7
 timed_stream = tweet_stream.select(
     to_timestamp('tweet.created_at', tweet_datetime_format).alias('timestamp'),
-    # 6
     explode(
         split('tweet.text', ' ')
     ).alias('word'))
-# 7 and 8
+# 8
 windowed = timed_stream \
     .withWatermark('timestamp', '1 minute') \
     .groupBy(window('timestamp', '10 minutes'), 'word')
