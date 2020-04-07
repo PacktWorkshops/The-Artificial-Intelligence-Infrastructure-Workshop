dc7d7149caa4d3803c67d2598280156bb3466f13
Bas Geerdink <bas@gdink.com>
2020-04-07 20:13:14 +0200

    updated notebooks and Python files of chapter 3 after review

--- Chapter03/Exercise03.03/spark_twitter.py
+++ Chapter03/Exercise03.03/spark_twitter.py
@@ -2,14 +2,14 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, window, to_timestamp
 from pyspark.sql.types import StructType, StructField, StringType
 
+# the schema to parse a tweet in JSON format; we only need two columns
+schema = StructType([StructField('created_at', StringType(), True),
+                     StructField('text', StringType(), True)])
+
 tweet_datetime_format = 'EEE MMM dd HH:mm:ss ZZZZ yyyy'
 
 # launch a Spark session
 spark = SparkSession.builder.appName('Packt').getOrCreate()
-
-# the schema to parse a tweet in JSON format; we only need two columns
-schema = StructType([StructField('created_at', StringType(), True),
-                     StructField('text', StringType(), True)])
 
 # get the raw data from a local socket
 raw_stream = spark.readStream.format('socket').option('host', 'localhost').option('port', 1234).load()
@@ -25,7 +25,6 @@ timed_stream = tweet_stream.select(
 # To display the tweets without windowing:
 # query = timed_stream.writeStream.outputMode('append').format('console').start()
 # query.awaitTermination()
-
 
 # create a sliding window of 1 minute with a slide of 10 seconds, with a 'slack time' of 2 seconds
 windowed = timed_stream \
